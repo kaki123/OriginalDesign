e8baf11e0efa2bc4c4e89394c0ef8110f57fa4ea
kaki123 <kakifung320@gmail.com>
2015-08-26 10:45:02 -0700

    second draft

--- OriginalDesign.pde
+++ OriginalDesign.pde
@@ -9,14 +9,14 @@ void draw()
   face();
 }
 void mousePressed() {
-   background(random(0,100),random(0,150),random(0,155));
+   background(random(224,250),random(220,255),random(220,255));
 }
 
 void face ()
 {
 	if(mouseX>200){
         ellipse(mouseX,mouseY,random(5,50),random(5,30));
-        fill(random(0,255), random(0,255), random(0,255));
+        fill(random(0,255), random(100,255), random(100,255));
     }
     if(mouseX<=200){
         rect(mouseX,mouseY,random(5,40),random(5,40),2);
--- build-tmp/source/originalDesign.java
+++ build-tmp/source/originalDesign.java
@@ -25,14 +25,14 @@ public void draw()
   face();
 }
 public void mousePressed() {
-   background(random(0,100),random(0,150),random(0,155));
+   background(random(224,250),random(220,255),random(220,255));
 }
 
 public void face ()
 {
 	if(mouseX>200){
         ellipse(mouseX,mouseY,random(5,50),random(5,30));
-        fill(random(0,255), random(0,255), random(0,255));
+        fill(random(0,255), random(100,255), random(100,255));
     }
     if(mouseX<=200){
         rect(mouseX,mouseY,random(5,40),random(5,40),2);
